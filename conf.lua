7f38fcf166c6f6c38377bc9d3d9acc3729f61a27
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2017-04-23 06:42:38 -0700

    Add a conf.lua

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.title = "Lunar Depot 38"
+    t.version = "0.10.2"
+    t.window.width = 800
+    t.window.height = 600
+    t.window.resizable = false
+    --t.window.icon = 'assets/images/icon.png'
+end
